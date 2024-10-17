7c036af0c8e54c21b1a8ba15848c4973b7ff0c28
OlonaMashalaba <olona.mashalaba@gmail.com>
2024-10-17 14:51:10 +0200

    Initial commit with Django

---
+++ mySite/blog/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+urlpatterns = [
+    path(
+        '', 
+        ListView.as_view(
+            queryset=Post.objects.all().order_by("-date")[:25],
+            template_name="blog.html"
+        ),
+        name='blog-home'
+    ),
+    path(
+        '<int:pk>/', 
+        DetailView.as_view(
+            model=Post,
+            template_name="post.html"
+        ),
+        name='post-detail'
+    ),
+]
